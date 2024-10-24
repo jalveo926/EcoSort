62b8530e02a92b6dcf49425c8747bb43558b5bb8
YSIK <jessicazh0103@gmail.com>
2024-10-24 17:10:27 -0500

    los botones para abrir los pop ups de los basureros ya funcionan

--- Interfaz-principal/script.js
+++ Interfaz-principal/script.js
@@ -1,10 +1,43 @@
-function openPopup(popupId) {
-    document.getElementById('overlay').style.display = 'block';
-    document.getElementById(popupId).style.display = 'block';
-}
+// Abre y cierra el boton de jugar
+document.getElementById("openJugar").onclick = function() {
+    document.getElementById("jugar").style.display = "block";
+};
 
-function closePopup() {
-    document.getElementById('overlay').style.display = 'none';
-    const popups = document.querySelectorAll('.popup');
-    popups.forEach(popup => popup.style.display = 'none');
-}
+document.getElementById("closeJugar").onclick = function() {
+    document.getElementById("jugar").style.display = "none";
+};
+
+// Abre y cierra el boton de aprende
+document.getElementById("openAprende").onclick = function() {
+    document.getElementById("aprende").style.display = "block";
+};
+
+document.getElementById("closeAprende").onclick = function() {
+    document.getElementById("aprende").style.display = "none";
+};
+
+// Abre y cierra el boton de créditos
+document.getElementById("openCreditos").onclick = function() {
+    document.getElementById("creditos").style.display = "block";
+};
+
+document.getElementById("closeCreditos").onclick = function() {
+    document.getElementById("creditos").style.display = "none";
+};
+
+// Manejo de los su pop ups
+const openBasurero = document.querySelectorAll(".openBasurero");
+const closeBasurero = document.querySelectorAll(".closeBasurero");
+
+openBasurero.forEach(button => {
+    button.onclick = function() {
+        const target = this.getAttribute("data-target");
+        document.getElementById(target).style.display = "block";
+    };
+});
+
+closeBasurero.forEach(button => {
+    button.onclick = function() {
+        this.parentElement.style.display = "none";
+    };
+});
